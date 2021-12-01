4ee878839efe0ac22a35b0cc31488287420ffaad
Enrique González Boza <enrgonboz@alum.us.es>
2021-12-01 16:24:04 +0100

    configuracion heroku dependencias 2

--- decide/decide/settings.py
+++ decide/decide/settings.py
@@ -71,7 +71,19 @@ MODULES = [
     'voting',
 ]
 
-BASEURL = 'http://localhost:8000'
+BASEURL = 'https://egc-enrgonboz.herokuapp.com'
+
+APIS = {
+    'authentication': 'http://10.5.0.1:8000',
+    'base': 'http://10.5.0.1:8000',
+    'booth': 'http://10.5.0.1:8000',
+    'census': 'http://10.5.0.1:8000',
+    'mixnet': 'http://10.5.0.1:8000',
+    'postproc': 'http://10.5.0.1:8000',
+    'store': 'http://10.5.0.1:8000',
+    'visualizer': 'http://10.5.0.1:8000',
+    'voting': 'http://10.5.0.1:8000',
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
