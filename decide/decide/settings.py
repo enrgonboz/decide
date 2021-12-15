ced55dd7b59418bebe1359b8c14fc977c466aa6a
enrgonboz <73030705+enrgonboz@users.noreply.github.com>
2021-12-15 16:25:15 +0100

    Update settings.py

--- decide/decide/settings.py
+++ decide/decide/settings.py
@@ -194,4 +194,8 @@ if os.path.exists("config.jsonnet"):
 
 INSTALLED_APPS = INSTALLED_APPS + MODULES
 
-django_heroku.settings(locals())
+NOSE_ARGS = [
+    '--with-xunit'
+]
+
+django_heroku.settings(locals(), test_runner=False)
